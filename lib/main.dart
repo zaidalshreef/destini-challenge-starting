3a49fb8aa34094fa7c7b6fdde8be82933c6f04f3
zaid <zaidalshreef@gmail.com>
2021-07-22 13:40:53 +0300

    Merge remote-tracking branch 'origin/master'

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 //TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
-
+int q = 0;
 void main() => runApp(Destini());
 
 class Destini extends StatelessWidget {
